0304a6ae4397676cb037e044347fd1654a1a75d6
arjunhkumar <s21008@students.iitmandi.ac.in>
2024-01-21 22:47:23 +0530

    v1.0

--- code/Container_X.java
+++ code/Container_X.java
@@ -1,9 +1,7 @@
-public class Container1 {
+class Container1 {
     InlineableField iField1;
     InlineableField iField2;
-    boolean flag;
 }
-
 class Container2 {
     InlineableField iField3;
 }
